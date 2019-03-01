cfea73ae2c49503281ea54c765f4f1e8859446c0
Jimi Wills <jimiwills@gmail.com>
2019-03-01 09:17:53 +0000

    better error messages

--- bindown.pl
+++ bindown.pl
@@ -19,18 +19,25 @@ sub bindown {
 	my $hex = '';
 
 	my @blocks = split /\n\s*\n/,$c;
-
+	
+	my $blockid = 0;
 	foreach(@blocks){
+		$blockid ++;
 		if(/^( {0,3})\S/m){
 			# text
 		}
 		else {
+			my $err = "ERR:\n$_\n======";
 			# code
-			s/;.*//g; # comments with ;
+			s/;.*$//gm; # comments with ;
+			$err .= "\ncomments removed\n$_\n======";
 			s/h\b//g;
+			$err .= "\nh removed\n$_\n======";
 			s/\b0x//g;
+			$err .= "\n0x removed\n$_\n======";
 			s/\s+//g;
-			die "$fn: illegal characters in $_"
+			$err .= "\nspace removed\n$_\n======";
+			die "$err\n\n$fn: illegal characters in block $blockid"
 				if /[^a-fA-f0-9]/;
 			$hex .= $_;
 		}
